0987235099e3b9ce2b8546c03b0a290dff59c0fd
rellaxa <ewg.nadeev@yandex.ru>
2023-12-20 00:59:03 +0300

    1.3. Вызов метода с аргументами. [#166086 #56021]

---
+++ src/main/java/oop/Jukebox.java
@@ -0,0 +1,18 @@
+package oop;
+
+public class Jukebox {
+    public void music(int position) {
+        switch (position) {
+            case 1 -> System.out.println("Пусть бегут неуклюже");
+            case 2 -> System.out.println("Спокойной ночи");
+            default -> System.out.println("Песня не найдена");
+        }
+    }
+
+    public static void main(String[] args) {
+        Jukebox ubex = new Jukebox();
+        ubex.music(1);
+        ubex.music(2);
+        ubex.music(5);
+    }
+}
