bda81a22f63d250da8ed5ea84a8312e5ab6a2bc0
rellaxa <ewg.nadeev@yandex.ru>
2024-01-03 17:53:14 +0300

    8. Date. Отображение даты. [#310017 #56093]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,15 +1,21 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+
 public class Item {
     private int id;
     private String name;
+    LocalDateTime created = LocalDateTime.now();
 
     public Item() {
-
     }
 
     public Item(String name) {
         this.name = name;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
     }
 
     public Item(int id, String name) {
