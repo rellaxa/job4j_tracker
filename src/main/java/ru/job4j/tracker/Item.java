b44ea12939db63c3715a94c8ff125555adbb521c
rellaxa <ewg.nadeev@yandex.ru>
2024-01-15 20:30:09 +0300

    10. toString [#310018 #56081]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,11 +1,13 @@
 package ru.job4j.tracker;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Item {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+    private LocalDateTime created = LocalDateTime.now();
     private int id;
     private String name;
-    LocalDateTime created = LocalDateTime.now();
 
     public Item() {
     }
@@ -17,6 +19,15 @@ public class Item {
     public Item(int id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{"
+                + "created=" + created.format(FORMATTER) + '\''
+                + ", id=" + id + '\''
+                + ", name='" + name + '\''
+                + '}';
     }
 
     public LocalDateTime getCreated() {
--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -8,5 +8,7 @@ public class StartUI {
         System.out.println("До форматирования: " + item.getCreated());
         System.out.println("После форматирования: "
                 + item.getCreated().format(DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss")));
+        item = new Item(1, "First");
+        System.out.println(item);
     }
 }
