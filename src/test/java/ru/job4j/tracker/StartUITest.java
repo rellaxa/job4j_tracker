093e1dba57e61e59332653df16b0b92269e31f64
rellaxa <ewg.nadeev@yandex.ru>
2024-01-28 18:57:26 +0300

    12.0. Написать тесты на StartUI. [#785 #56040]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -5,56 +5,51 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class StartUITest {
-
     @Test
-    void whenAddItem() {
-        String[] answers = {"Fix PC", "Fix Bug"};
-        Input input = new MockInput(answers);
+    void whenCreateItem() {
+        Input input = new MockInput(
+                new String[] {"0", "Item name", "1"}
+        );
         Tracker tracker = new Tracker();
-        StartUI.createItem(input, tracker);
-        StartUI.createItem(input, tracker);
-        Item created = tracker.findAll()[0];
-        Item expected = new Item("Fix PC");
-        assertThat(created.getName()).isEqualTo(expected.getName());
-        created = tracker.findAll()[1];
-        expected = new Item("Fix Bug");
-        assertThat(created.getName()).isEqualTo(expected.getName());
-    }
-
-    @Test
-    void whenErrorAddItem() {
-        Input input = new ConsoleInput();
-        Tracker tracker = new Tracker();
-        StartUI.createItem(input, tracker);
-        Item created = tracker.findAll()[0];
-        Item expected = new Item("Fix PC");
-        assertThat(created.getName()).isEqualTo(expected.getName());
-    }
-
-    @Test
-    void whenReplaceItem() {
-        Tracker tracker = new Tracker();
-        Item item = new Item("new item");
-        tracker.add(item);
-        String[] answers = {
-                String.valueOf(item.getId()), /* id сохраненной заявки в объект tracker. */
-                "edited item"
+        UserAction[] actions = {
+                new CreateAction(),
+                new ExitAction()
         };
-        StartUI.replaceItem(new MockInput(answers), tracker);
-        Item edited = tracker.findById(item.getId());
-        assertThat(edited.getName()).isEqualTo("edited item");
+        new StartUI().init(input, tracker, actions);
+        assertThat(tracker.findAll()[0].getName()).isEqualTo("Item name");
+    }
+
+    @Test
+    void whenReplaceAction() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("Replaced item")); /* Добавляется в tracker новая заявка */
+        String replacedName = "New item name";
+        Input input = new MockInput(
+                new String[]{"0",
+                        String.valueOf(item.getId()),
+                        replacedName, "1"}
+        );
+        UserAction[] actions = {
+                new ReplaceAction(),
+                new ExitAction()
+        };
+        new StartUI().init(input, tracker, actions);
+        assertThat(tracker.findById(item.getId()).getName()).isEqualTo(replacedName);
     }
 
     @Test
     void whenDeleteItem() {
         Tracker tracker = new Tracker();
-        Item item = new Item("Deleted Item");
-        tracker.add(item);
-        String[] answers = {
-                String.valueOf(item.getId())
+        Item item = tracker.add(new Item("Deleted item"));
+        Input input = new MockInput(
+                new String[]{"0",
+                        String.valueOf(item.getId()), "1"}
+        );
+        UserAction[] actions = {
+                new DeleteAction(),
+                new ExitAction()
         };
-        StartUI.deleteItem(new MockInput(answers), tracker);
-        Item deleted = tracker.findById(item.getId());
-        assertThat(deleted).isNull();
+        new StartUI().init(input, tracker, actions);
+        assertThat(tracker.findById(item.getId())).isNull();
     }
 }
