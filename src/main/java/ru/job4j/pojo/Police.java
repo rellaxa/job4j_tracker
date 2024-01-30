2c1077c581be85005f00226a9eaa94cb13756c07
rellaxa <ewg.nadeev@yandex.ru>
2024-01-30 22:31:43 +0300

    package ru.job4j.pojo;

---
+++ src/main/java/ru/job4j/pojo/Police.java
@@ -0,0 +1,16 @@
+package ru.job4j.pojo;
+
+import java.util.Date;
+
+public class Police {
+    public static void main(String[] args) {
+        License license = new License();
+        license.setOwner("Evgenii Nadeev");
+        license.setModel("Porsche Panamera");
+        license.setCode("xx777x");
+        license.setCreated(new Date());
+
+        System.out.println(license.getOwner() + " has a car - " + license.getModel() + " : " + license.getCode()
+                + " : " + license.getCreated());
+    }
+}
