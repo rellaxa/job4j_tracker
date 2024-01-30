f4aedbd1d9133be450954d0f4e810de96322c74b
rellaxa <ewg.nadeev@yandex.ru>
2024-01-30 22:44:09 +0300

    package ru.job4j.condition;

---
+++ src/main/java/ru/job4j/condition/Max.java
@@ -0,0 +1,21 @@
+package ru.job4j.condition;
+
+public class Max {
+    public static int max(int left, int right) {
+        int max = right;
+        if (left < 0) {
+            left = -left;
+        }
+        if (right < 0) {
+            right = -right;
+        }
+        if (left >= right) {
+            max = left;
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(max(-2, -1));
+    }
+}
