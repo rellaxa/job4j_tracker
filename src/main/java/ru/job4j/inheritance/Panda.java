7cd7f2cd32718b42d8ed2e0535518d608b2acc5c
rellaxa <ewg.nadeev@yandex.ru>
2024-01-30 22:41:46 +0300

    package ru.job4j.inheritance;

---
+++ src/main/java/ru/job4j/inheritance/Panda.java
@@ -0,0 +1,12 @@
+package ru.job4j.inheritance;
+
+public class Panda extends Animal {
+    @Override
+    public void instanceInvoke() {
+        System.out.println("Вызов метода экземпляра Panda");
+    }
+
+    public static void staticInvoke() {
+        System.out.println("Вызов статического метода Panda");
+    }
+}
