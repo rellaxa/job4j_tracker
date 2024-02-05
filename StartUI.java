fcb5eb66b96a2660be6f9e174353a9f14f304c20
Roman_Kulianin <148569591+rellaxa@users.noreply.github.com>
2024-02-05 20:59:13 +0300

    Add files via upload

---
+++ StartUI.java
@@ -0,0 +1,49 @@
+package ru.job4j.tracker;
+
+import ru.job4j.tracker.action.*;
+
+public class StartUI {
+    private final Output out;
+
+    public StartUI(Output output) {
+        this.out = output;
+    }
+
+    public void init(Input input, Tracker tracker, UserAction[] actions) {
+        boolean run = true;
+        while (run) {
+            showMenu(actions);
+            int select = input.askInt("Выбрать: ");
+            if (select < 0 || select >= actions.length) {
+                out.println("Неверный ввод, вы можете выбрать: 0 .. " + (actions.length - 1));
+                continue;
+            }
+            UserAction action = actions[select];
+            run = action.execute(input, tracker);
+        }
+    }
+
+    private void showMenu(UserAction[] actions) {
+        out.println("Меню:");
+        for (int index = 0; index < actions.length; index++) {
+            out.println(index + ". " + actions[index].name());
+        }
+    }
+
+    public static void main(String[] args) {
+        Output output = new ConsoleOutput();
+        Input input = new ValidateInput(output, new ConsoleInput());
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new Create(output),
+                new FindAll(output),
+                new Replace(output),
+                new Delete(output),
+                new FindById(output),
+                new FindByName(output),
+                new Exit(output)
+        };
+
+        new StartUI(output).init(input, tracker, actions);
+    }
+}
