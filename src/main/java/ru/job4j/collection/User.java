00ee8206c47683dcfefbe061f4e202895470e673
rellaxa <ewg.nadeev@yandex.ru>
2024-02-26 15:20:06 +0300

    1. Организовать сортировку User [#10034 #56002]

---
+++ src/main/java/ru/job4j/collection/User.java
@@ -0,0 +1,36 @@
+package ru.job4j.collection;
+
+import java.util.Objects;
+
+public class User implements Comparable<User> {
+    private String name;
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public int compareTo(User o) {
+        int compare = name.compareTo(o.name);
+        return compare == 0 ? Integer.compare(age, o.age) : compare;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object == null || getClass() != object.getClass()) {
+            return false;
+        }
+        User user = (User) object;
+        return age == user.age && Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+}
