99bb36432c84a5203fed65de6658d11cb2b2977d
relaxa <ewg.nadeev@yandex.ru>
2024-03-23 22:51:49 +0300

    7. Уникальность элементов и сортировка. [#110225 #55863]

--- src/test/java/ru/job4j/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -7,10 +7,10 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class ProfilesTest {
+public class ProfilesTest {
 
     @Test
-    void whenTestMethodCollect() {
+    public void whenTestMethodCollect() {
         Address first = new Address("City1", "Street1", 1, 1);
         Address second = new Address("City2", "Street2", 2, 2);
         Address third = new Address("City3", "Street3", 3, 3);
@@ -20,6 +20,27 @@ class ProfilesTest {
                 new Profile(third)
         );
         List<Address> result = Profiles.collect(profiles);
+        List<Address> expected = Arrays.asList(first, second, third);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenTestMethodCollectSortWithoutDuplicate() {
+        Address first = new Address("City1", "Street1", 1, 1);
+        Address second = new Address("City2", "Street2", 2, 2);
+        Address third = new Address("City3", "Street3", 3, 3);
+        Address fourth = new Address("City1", "Street1", 1, 1);
+        Address fifth = new Address("City2", "Street2", 2, 2);
+        Address seventh = new Address("City3", "Street3", 3, 3);
+        List<Profile> profiles = Arrays.asList(
+                new Profile(second),
+                new Profile(fourth),
+                new Profile(fifth),
+                new Profile(first),
+                new Profile(seventh),
+                new Profile(third)
+        );
+        List<Address> result = Profiles.collectSortWithoutDuplicate(profiles);
         List<Address> expected = Arrays.asList(first, second, third);
         assertThat(result).isEqualTo(expected);
     }
