1ddd03c0939e11bb90e0aa8f986430501c3c332e
relaxa <ewg.nadeev@yandex.ru>
2025-02-21 22:01:21 +0300

    1. Lombok. Настройка IDEA [#504888 #334600]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 @Data
 public class Item implements Comparable<Item> {
@@ -11,7 +12,7 @@ public class Item implements Comparable<Item> {
     private static final DateTimeFormatter FORMATTER =
             DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
 
-    private LocalDateTime created = LocalDateTime.now();
+    private LocalDateTime created = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
 
     private int id;
 
@@ -32,5 +33,14 @@ public class Item implements Comparable<Item> {
     @Override
     public int compareTo(Item o) {
         return Integer.compare(id, o.id);
+    }
+
+    @Override
+    public String toString() {
+        return "Item{"
+                + "id = " + id + '\''
+                + ", name = '" + name + '\''
+                + ", created = " + created.format(FORMATTER) + '\''
+                + '}';
     }
 }
